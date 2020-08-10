768df7157378040fc522ef9a64231daac601839c
yeoj1n <yeojinkim94@gmail.com>
2020-08-10 23:17:54 +0900

    라우팅 추가

---
+++ vue-project/src/components/Sample.vue
@@ -0,0 +1,34 @@
+<template>
+    <div><span>{{test()}}</span></div>
+</template>
+
+<script>
+export default {
+    name: 'Sample',
+    data() {
+        return {
+            firstName: 'Foo',
+            lastName: 'Bar',
+            fullName: 'Foo Bar'
+        }
+        
+    },
+    computed: {
+        /*fullName: {
+            get: function() {
+                return this.firstName + ' ' + this.lastName
+            },
+            set: function(newValue) {
+                var names = newValue.split(' ')
+                this.firstName = names[0]
+                this.lastName = names[names.length - 1]
+            }
+        }*/
+    },
+    methods: {
+        test: function() {
+            this.fullName.set = 'Jone Doe'
+        }
+    }
+}
+</script>
--- vue-project/src/routes/index.js
+++ vue-project/src/routes/index.js
@@ -5,8 +5,10 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 // 만약 이전 글에서 alias 경로 단축을 설정했다면 다음과 같이 절대경로로 작성가능하다.
+import Sample from '@/components/Sample.vue'
 import Main from '@/components/Main.vue'
 import Shop from '@/components/shop/Shop.vue'
+import NotFound from '@/components/NotFound.vue'
 
 // ES6 Named export 방식 
 export const router = new VueRouter({
@@ -14,13 +16,23 @@ export const router = new VueRouter({
     routes: [
         {
             path: '/',
+            component: Sample,
+            name: 'Sample'
+        },
+        {
+            path: '/main',
             component: Main,
             name: 'Main' // name을 적어주면 나중에 프로그래밍 방식의 라우터 전환이 편해진다.
         },
         {
-            path: '/shop',
+            path: '/shop/:id',
             component: Shop,
             name: 'Shop'
+        },
+        {
+            path: '*',
+            component: NotFound,
+            name: 'NotFound'
         }
     ]
   })

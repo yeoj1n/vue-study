107cfd6cfef33bdcf2243ca11c78b41cabb874be
yeoj1n <yeojinkim94@gmail.com>
2020-08-13 00:39:39 +0900

    config 수정:미사용 변수, 함수에 대해 검사하는 eslint문법 제외

--- vue-project/vue.config.js
+++ vue-project/vue.config.js
@@ -1,16 +1,19 @@
 // 경로: 프로젝트 루트 경로(pakge.json이 위치한 디렉토리)
 // node의 path 모듈을 가져옵니다.
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    // 웹팩 설정
-    configureWebpack: {
-        // resolve.alias는 모듈을 더 쉽게 import, require 하게 만듭니다.
-        resolve: {
-            alias: {
-                // '@'는 현재 프로젝트의 client/src/까지의 최종 경로를 의미합니다.
-                '@': path.join(__dirname, 'src/')
-            }
-        }
-    }
-}
+  // 웹팩 설정
+  configureWebpack: {
+    // resolve.alias는 모듈을 더 쉽게 import, require 하게 만듭니다.
+    resolve: {
+      alias: {
+        // '@'는 현재 프로젝트의 client/src/까지의 최종 경로를 의미합니다.
+        "@": path.join(__dirname, "src/"),
+      },
+    },
+  },
+  devServer: {
+    overlay: false,
+  }
+};
